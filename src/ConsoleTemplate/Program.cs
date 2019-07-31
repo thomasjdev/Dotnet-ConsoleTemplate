b9e16a386f7386b42554a22a3219e7ff163aa3a5
Thomas Anderson <thomas@tjanderson.net>
2019-07-31 16:33:31 -0700

    Creating a robust console app tempate

---
+++ src/ConsoleTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
